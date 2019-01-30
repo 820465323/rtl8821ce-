b467726a37b816516c9969d32518d26715a031eb
zzw <17319132391zzw.com>
2019-01-30 13:50:33 +0800

    rtl8821ce网卡配置

---
+++ rtl8821ce2/.8821ce.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/media/zzw/U6G/rtl8821ce/8821ce.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /media/zzw/U6G/rtl8821ce/8821ce.ko /media/zzw/U6G/rtl8821ce/8821ce.o /media/zzw/U6G/rtl8821ce/8821ce.mod.o ;  true
