d167a505cba4e47d754b14bea7e667153ad54631
zhangzhouwang <17319132391@163.com>
2019-02-25 19:04:13 +0800

    rtl8821ce

---
+++ rtl8821ce/rtl8821ce/.8821ce.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zzw/下载/rtl8821ce/rtl8821ce/8821ce.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/zzw/下载/rtl8821ce/rtl8821ce/8821ce.ko /home/zzw/下载/rtl8821ce/rtl8821ce/8821ce.o /home/zzw/下载/rtl8821ce/rtl8821ce/8821ce.mod.o ;  true
